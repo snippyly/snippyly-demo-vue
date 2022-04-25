d589451d13fe97353dee1299e973c712c5420ef3
vivekk-snippyly <vivek@snippyly.com>
2022-04-25 15:23:03 +0530

    Added 'snippyly-comments'. Updated snippyly to 1.0.29.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <snippyly-cursor></snippyly-cursor>
+    <snippyly-comments></snippyly-comments>
     <snippyly-comment-tool>
       <div class="add-comment-btn">
         <img
@@ -45,7 +46,14 @@ import { Users } from "./users";
 import loadSnippyly from "./loadSnippyly";
 
 let selectedUser;
+
+/**
+ * @type {import('@snippyly/types').Snippyly}
+ */
 var Snippyly;
+/**
+ * @type {import('@snippyly/types').Snippyly}
+ */
 var client;
 
 const initSnippyly = async () => {
