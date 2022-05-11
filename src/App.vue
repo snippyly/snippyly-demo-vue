1c93e1b21006f101c34919a28cf57e7f3e77759c
vivekk-snippyly <vivek@snippyly.com>
2022-05-11 17:53:42 +0530

    Updated snippyly to 1.0.33. Added text comments feature.

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,91 @@
         <span>{{ box }}</span>
       </div>
     </div>
+    <div class="text-comments-container">
+      <h1>Google Docs Style Comments</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint commodi
+        architecto aliquid explicabo exercitationem nemo reprehenderit, rerum
+        autem nostrum aperiam corrupti ab, cumque tenetur incidunt dolor vel,
+        atque qui! Veritatis itaque omnis est in architecto eligendi temporibus
+        ipsa consectetur. Pariatur dolor beatae maxime suscipit repellat
+        consectetur nulla placeat, iusto ipsa labore. Eaque, ab quod! Illum
+        quibusdam ipsa possimus, aliquam dolore laborum dicta iusto temporibus
+        iste dolores officia minima ad porro ducimus facere esse, tenetur
+        exercitationem quae vitae? Soluta dicta sed ullam debitis accusamus
+        necessitatibus sequi perspiciatis. Exercitationem at voluptatum magni,
+        nisi repellat unde a delectus! Neque quod quae necessitatibus nisi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint commodi
+        architecto aliquid explicabo exercitationem nemo reprehenderit, rerum
+        autem nostrum aperiam corrupti ab, cumque tenetur incidunt dolor vel,
+        atque qui! Veritatis itaque omnis est in architecto eligendi temporibus
+        ipsa consectetur. Pariatur dolor beatae maxime suscipit repellat
+        consectetur nulla placeat, iusto ipsa labore. Eaque, ab quod! Illum
+        quibusdam ipsa possimus, aliquam dolore laborum dicta iusto temporibus
+        iste dolores officia minima ad porro ducimus facere esse, tenetur
+        exercitationem quae vitae? Soluta dicta sed ullam debitis accusamus
+        necessitatibus sequi perspiciatis. Exercitationem at voluptatum magni,
+        nisi repellat unde a delectus! Neque quod quae necessitatibus nisi.
+      </p>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+        reiciendis optio fuga?
+      </h3>
+      <ul>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>
+          Consequat exercitation consequat et laborum do consequat nostrud
+          adipisicing sunt laboris id eiusmod pariatur.
+        </li>
+        <li>Non non aliquip proident amet.</li>
+        <li>
+          Et labore sit amet ad do labore cupidatat tempor ad cillum excepteur
+          anim.
+        </li>
+        <li>
+          Consequat duis excepteur aute sit ipsum qui id dolore est cupidatat.
+        </li>
+        <li>
+          Incididunt ea veniam minim aute sunt exercitation ad amet non anim ut.
+        </li>
+      </ul>
+      <p>
+        Exercitation minim ad
+        <strong
+          >mollit esse cupidatat cillum tempor esse deserunt mollit aliquip.
+          Occaecat consectetur proident do deserunt nostrud pariatur
+          quis</strong
+        >
+        minim eiusmod eu cillum.
+        <i
+          >Excepteur sunt deserunt et duis exercitation
+          <b>nostrud commodo dolor ipsum irure ea.</b> Sit non dolor nisi
+          occaecat consectetur labore excepteur consectetur. Reprehenderit</i
+        >
+        labore ad deserunt nulla dolor veniam cupidatat nostrud consectetur.
+        Cillum ea minim consequat cillum aliquip
+        <u
+          >ad ea elit mollit eu culpa et eu. Amet Lorem deserunt voluptate
+          aliqua</u
+        >
+        esse tempor culpa mollit exercitation veniam exercitation. Consectetur
+        cupidatat <b>quis minim aliqua. Irure esse id fugiat</b> occaecat nulla
+        ipsum excepteur ea sit aliquip dolore et culpa. Id in officia dolore
+        aliqua ullamco anim
+        <s
+          >adipisicing deserunt reprehenderit et.
+          <b
+            >Mollit ea consequat exercitation
+            <u>deserunt cupidatat nulla irure</u> Lorem quis deserunt.</b
+          >
+          Anim id veniam id aliquip occae</s
+        >cat cupidatat duis labore aliquip in sint. Consectetur ea sit velit eu
+        culpa est eiusmod nisi do aliquip mollit officia tempor aliqua. Ea
+        cupidatat <sub>consequat laboris ipsum</sub> consequat est nostrud sint.
+        Minim officia occaecat <sup>sint culpa</sup> consequat. Esse amet
+        eiusmod exercitation irure ullamco enim et sunt ex aliquip non.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -65,6 +150,10 @@ const initSnippyly = async () => {
     urlAllowList: [], // To allow snippyly in specific screens only
   });
   console.log("init Snippyly", client);
+
+  // To enable text comment feature
+  const commentElement = client.getCommentElement();
+  commentElement.enableTextCommentButton(true);
 
   if (getUser()) {
     selectedUser = getUser();
@@ -193,5 +282,9 @@ snippyly-comment-tool .add-comment-btn {
 snippyly-comment-tool .add-comment-btn img {
   width: 1.5rem;
   height: 1.5rem;
+}
+
+.text-comments-container {
+  padding: 24px;
 }
 </style>
